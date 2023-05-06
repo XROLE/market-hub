d71425b7a38e22eea6c77344060c32cc2f22cb3a
xrole <xrolediamond@gmail.com>
2023-05-06 11:18:36 +0100

    create search bar

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:market_hub/presentation/home/widgets/app_searchbar.dart';
 import 'package:market_hub/presentation/home/widgets/bottom_nav_bar.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,9 +9,12 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Welcome to market hub"),),
-      bottomNavigationBar: BottomNavBar(),
+    return  Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: 70, left: 20, right: 20, bottom: 20),
+        child: Column(children: [
+          AppSearchBar()])),
+      bottomNavigationBar: const BottomNavBar(),
     );
   }
 }
